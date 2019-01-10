d10758728473ac011698e5d78e86f2fb7b212a65
qmylzx <qmylzx@outlook.com>
2019-01-10 15:27:45 +0800

    UI

---
+++ server/src/main/java/base/pojo/User.java
@@ -0,0 +1,56 @@
+package base.pojo;
+
+import com.google.gson.Gson;
+
+public class User {
+    private int id;
+    private String sex;
+    private String education;
+    private String job;
+    private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
